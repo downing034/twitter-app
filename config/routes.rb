cb1f4d0fbe293c9227f219e56be91c34ede7eaca
Paul Downing <downing034@gmail.com>
2019-09-02 13:30:32 -0500

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'dashboard#home'
-  get '/help', to: 'dashboard#help'
-  get '/about', to: 'dashboard#about'
-  get '/contact', to: 'dashboard#contact'
-
-  get '/signup', to: 'users#new'
-
+  get '/help',     to: 'dashboard#help'
+  get '/about',    to: 'dashboard#about'
+  get '/contact',  to: 'dashboard#contact'
+  get '/signup',   to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   # resources :posts
-  # resources :users
+
 end
