77fe7495c64d3030378d7a19dd9321afa8987ccc
Paul Downing <downing034@gmail.com>
2019-09-02 17:14:23 -0500

    first round of basic auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to the Twitter App!'
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'dashboard#home'
-  get '/help',     to: 'dashboard#help'
-  get '/about',    to: 'dashboard#about'
-  get '/contact',  to: 'dashboard#contact'
-  get '/signup',   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get '/help',      to: 'dashboard#help'
+  get '/about',     to: 'dashboard#about'
+  get '/contact',   to: 'dashboard#contact'
+  get '/signup',    to: 'users#new'
+  post '/signup',    to: 'users#create'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # resources :posts
 
