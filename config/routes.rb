14f96e07749c29349371c7ceea3addfab9a6fe7d
Paul Downing <downing034@gmail.com>
2019-09-01 22:00:23 -0500

    add seom static pages with base content

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root 'dashboard#home'
   get 'dashboard/home'
-
   get 'dashboard/help'
+  get 'dashboard/about'
 
   resources :posts
   resources :users
-
-  root 'users#index'
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,14 +1,31 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Twitter App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get dashboard_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get dashboard_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
+  test "should get about" do
+    get dashboard_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
 end
